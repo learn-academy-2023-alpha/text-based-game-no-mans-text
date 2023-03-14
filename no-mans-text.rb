82ccefe473ef9751559b2f74d72ee1749212661e
csaument <c.s.aument@gmail.com>
2023-03-14 13:17:41 -0700

    minor updates to text content and formatting

--- no-mans-text.rb
+++ no-mans-text.rb
@@ -6,6 +6,7 @@
       # Allow the player to track named objects
       # Allow the player to collect items through exploration and combat
       # Allow the player to develop and use various skills
+      # Track different body parts independently
 class Player
       attr_accessor :name, :health, :life_support, :skills, :equipment, :known_things
       def initialize(name)
@@ -49,6 +50,7 @@ end
       # Provide interaction between the plants and their environment/planet
             # eg Waterworlds bias instantiation of algea, kelp, and seaweed
             # eg Growth rate, with interaction from predators
+      # Create structural elements (roots, leaves, trunk, etc)
 class Plant
       attr_accessor :name, :size, :color, :type, :flower, :edible, :poisonous, :hostility
       def initialize()
@@ -69,12 +71,15 @@ end
       # Provide a means of distiguishing animal species from a specific animal
       # Create procreation methods and rates
       # Tune generation of arms/legs/heads to bias towards realistic/interesting options
+      # Allow multiple colors on different body parts
+      # Track different body parts independently
 class Animal
       attr_accessor :name, :size, :color, :type, :legs, :arms, :heads, :diet, :edible, :poisonous, :hostility
       def initialize()
           @name = nil
           @size = ['tiny', 'small', 'medium', 'large', 'huge'].sample
           @color = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black', 'white', 'brown'].sample
+          @surface = ['skin', 'fur', 'scales', 'feathers', 'mucous'].sample
           @type = ['fish', 'amphibian', 'reptile', 'mammal', 'bird', 'invertebrae'].sample
           @legs = rand(5)
           @arms = rand(5)
@@ -103,6 +108,12 @@ class Place
       end
 end
 
+# Create a class for procedurally generating minerals
+# Expansion ideas:
+      # Allow harvesting of mineral resources
+      # Provide additional utility in construction/manufacturing using the minerals
+      # Create rarety
+      # Create realistic formation of geological minerals
 class Mineral
       attr_accessor :name, :type, :color, :hardness, :melting_point
       def initialize()
@@ -137,12 +148,16 @@ def explore_planet (planet)
             index = rand (planet.animals.length - 1)
             encounter_animal = planet.animals[index]
             if encounter_animal.name.nil?
-                  puts "You have encountered a new animal species! This is one for the history books now closely but carefully note these characteristics about the animal. It is #{encounter_animal.size} size, With a #{encounter_animal.color} color, This animal has #{encounter_animal.legs} legs, they have #{encounter_animal.heads} head/heads.they have #{encounter_animal.arms} arms. It appears to be a species of #{encounter_animal.type}, It has a diet of #{encounter_animal.diet}." 
-                  puts 'what would you like to name your new discovery'
+                  puts 'You have encountered a new animal species! This is one for the history books! You carefully note these characteristics of the animal.'
+                  puts "It is #{encounter_animal.size}, with #{encounter_animal.color} #{encounter_animal.surface}."
+                  puts "This animal has #{encounter_animal.legs == 0 ? 'no legs' : encounter_animal.legs == 1 ? 'one leg' : encounter_animal.legs + ' legs'}, #{encounter_animal.heads == 0 ? 'no head' : encounter_animal.heads == 1 ? 'one head' : encounter_animal.head + ' heads'}, and #{encounter_animal.arms == 0 ? 'no arms' : encounter_animal.arms == 1 ? 'one arm' : encounter_animal.arms + ' arms'}."
+                  puts "It appears to be a species of #{encounter_animal.type}. It has a diet of #{encounter_animal.diet}."
+                  puts "\n"
+                  puts 'What would you like to name your new discovery'
                   encounter_animal.name = gets.chomp
                   planet.animals[index].name = encounter_animal.name
             else
-                  puts "You have encountered an animal! You recognize from it's  #{encounter_animal.size} size, #{encounter_animal.color} color, #{encounter_animal.legs} leg, #{encounter_animal.heads} head/heads, and #{encounter_animal.arms} arms. It appears to be a #{encounter_animal.name}."
+                  puts "You have encountered an animal! You recognize from it's  #{encounter_animal.size} size, #{encounter_animal.color} color, #{encounter_animal.legs == 0 ? 'no legs' : encounter_animal.legs == 1 ? 'one leg' : encounter_animal.legs + ' legs'}, #{encounter_animal.heads == 0 ? 'no head' : encounter_animal.heads == 1 ? 'one head' : encounter_animal.head + ' heads'}, and #{encounter_animal.arms == 0 ? 'no arms' : encounter_animal.arms == 1 ? 'one arm' : encounter_animal.arms + ' arms'}. It appears to be a #{encounter_animal.name}."
             end
 
             if 4 < planet.hostility + encounter_animal.hostility
@@ -151,15 +166,15 @@ def explore_planet (planet)
       when 26..60 # Plant encounter
             index = rand (planet.plants.length - 1)
             encounter_plant = planet.plants[index]
-            plant_flower = encounter_plant.flower ? 'has flowers': 'does not have flowers'
             if encounter_plant.name.nil?
-                  puts "You have found a unique plant species unlike any on earth! carefully note these characteristics about the plant. It is #{encounter_plant.size} size, With a #{encounter_plant.color} color, This plant #{plant_flower}, It appears to be a species of #{encounter_plant.type}." 
+                  puts 'You have found a unique plant species unlike any on earth! You carefully note these characteristics about the plant.'
+                  puts "It is #{encounter_plant.size} size, With a #{encounter_plant.color} color, This plant #{encounter_plant.flower ? 'has flowers': 'does not have flowers'}, It appears to be a species of #{encounter_plant.type}."
                   puts 'what would you like to name your new discovery'
                   encounter_plant.name = gets.chomp
                   planet.plants[index].name = encounter_plant.name
             else
-                  puts "You have found a familiar plant! based on these characteristics about the plant. You recognize it to be a #{encounter_plant.name}! It is #{encounter_plant.size} with a #{encounter_plant.color} color, This plant #{encounter_plant.flower ? 'has flowers': 'does not have flowers'}, It appears to be a species of #{encounter_plant.type}." 
-            end   
+                  puts "You have found a familiar plant! Based on its characteristics, you recognize it to be a #{encounter_plant.name}! It is #{encounter_plant.size} with a #{encounter_plant.color} color, This plant #{encounter_plant.flower ? 'has flowers': 'does not have flowers'}, It appears to be a species of #{encounter_plant.type}." 
+            end
 
             if 4 < planet.hostility + encounter_plant.hostility
                   combat_encounter (encounter_plant)
@@ -267,7 +282,11 @@ $new_player = Player.new(gets.chomp)
 # Start planet is created and passed to various functions for reference
 start_planet = Planet.new()
 
-puts "Welcome, #{$new_player.name}. You have landed onto a new and beautiful planet. The planet has a #{start_planet.color} color. The temperature is #{start_planet.temperature} degrees Fahrenheit. There are many animals of various hostility levels, please be careful. Explore to discover new life and resources. This planet has #{start_planet.water}% of water."
+puts "Welcome, #{$new_player.name}. You have landed onto a new and beautiful planet. The planet has a #{start_planet.color} color. The temperature is #{start_planet.temperature} degrees Fahrenheit. This planet has #{start_planet.water}% of water."
+puts 'What would you like to call this new world?'
+start_planet.name = gets.chomp
+
+puts 'There are many animals of various hostility levels, please be careful. Explore to discover new life and resources.'
 
 # Do..while loop to process gameplay
 # Expansion ideas:
@@ -277,6 +296,8 @@ puts "Welcome, #{$new_player.name}. You have landed onto a new and beautiful pla
       # Provide an option to travel to a known planet
       # Provide a way to view known planets/places/aliens/animals/plants
       # Provide a way to save and load
+      # Provide a way to trade resources
+      # Provide an initial start screen to start a new game, load a previous game, or modify settings
 loop do 
       explore_planet start_planet
 
